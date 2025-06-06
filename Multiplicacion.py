a0e7dea6978e461b75a65a94f55274e33a71a7e1
XxGaston90xX <wolmangaston@gmail.com>
2025-06-06 01:47:57 -0300

    Multiplicacion

---
+++ Multiplicacion.py
@@ -0,0 +1,7 @@
+# Definir multiplicacion con 3 parametros y resultados 
+def multiplicacion(a, b, c):
+    return a * b * c
+
+# El resultados 24
+resultado = multiplicacion(2, 3, 4)
+print("El resultado de la multiplicación es:", resultado)
