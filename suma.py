76e7d51222e4d60b14218bbb2d330a6983deadc8
XxGaston90xX <wolmangaston@gmail.com>
2025-06-06 01:28:10 -0300

    Suma

---
+++ suma.py
@@ -0,0 +1,7 @@
+# Crear un programa de suma con 3 parámetros y resultados.-
+def suma(a, b, c):
+    return a + b + c
+
+# Resultado de suma es 10 !
+resultado = suma(5, 3, 2)
+print("La suma es:", resultado)
